0bd8cdff633b3ada8657d31d9df76df984716641
Miguel Aguilar <migueleduagui@gmail.com>
2021-10-20 06:41:59 -0300

    Agregamos un componente Menu y un componente Order a la capeta components

---
+++ src/components/Order.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Order.scss';
+
+const Order = () => {
+    return(
+        <div class="order">
+            <p>
+                <span>03.25.21</span>
+                <span>6 articles</span>
+            </p>
+            <p>
+                 <span>$560.00</span>
+            </p>
+        </div>
+    );
+};
+
+export default Order;
