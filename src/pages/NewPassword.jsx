209610a6f15a0a0d3138da7b1e4bd35103f5aaff
Miguel Aguilar <migueleduagui@gmail.com>
2021-10-21 21:56:00 -0300

    Agregamos componente NewPassword a la carpeta Pages

---
+++ src/pages/NewPassword.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../styles/NewPassword.scss';
+
+const NewPassword = () => {
+    return(
+        <div className="login">
+            <div className="form-container">
+                <img 
+                    src="./src/static/logos/logo_yard_sale.svg"
+                    alt="logo"
+                    className="logo"
+                />
+                <h1 className="title">
+                    Create a new password
+                </h1>
+                <p className="subtitle"> 
+                    Enter a new password for your account.
+                </p>
+                <form action="/" className="form">
+                    <label htmlFor="password">
+                        Password
+                    </label>
+                    <input 
+                        type="password"
+                        id="password" 
+                        placeholder="******"
+                        className="input input-password"    
+                    />
+                    <label htmlFor="password">
+                        Password
+                    </label>
+                    <input 
+                        type="password"
+                        id="password" 
+                        placeholder="******"
+                        className="input input-password"    
+                    />
+                    <input type="submit"
+                        value="Confirm"
+                        className="primary-button login-button"     
+                    />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default NewPassword;
