71ad53a366cb32aab9e123254100a1901e8704af
Miguel Aguilar <migueleduagui@gmail.com>
2021-10-21 23:03:41 -0300

    Configuramos el componenete App con la ruta de los componentes previamente añadidos

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,6 +6,13 @@ import NotFound from '../pages/NotFound';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
+
 
 const App = () => {
     return(
@@ -15,6 +22,12 @@ const App = () => {
                     <Route exact path="/" component={Home}/>
                     <Route exact path="/login" component={Login}/>
                     <Route exact path="/recovery-password" component={RecoveryPassword}/>
+                    <Route exact path="/send-email" component={SendEmail}/>
+                    <Route exact path="/new-password"  component={NewPassword}/>
+                    <Route exact path="/account" component={MyAccount}/>
+                    <Route exact path="/signup" component={CreateAccount}/>
+                    <Route exact path="/checkout" component={Checkout}/>
+                    <Route exact path="/orders" component={Orders}/>
                     <Route path="*" component={NotFound}/>
                 </Switch>
             </Layout>    
