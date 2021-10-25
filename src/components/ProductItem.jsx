9b11314ab3564bfde8c9e9d8a8abaf4a4fd63c67
Miguel Aguilar <migueleduagui@gmail.com>
2021-10-25 14:40:23 -0300

    instalamos axios, instalamos babel/plugin-transform-runtime, creamos un hook de efecto y luego creamos un custom hook para trabajar con el api de la tienda y poder desplegar los elementos en venta de la seccion principal

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 import '@styles/ProductItem.scss';
 import addToCart from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
+const ProductItem = ({product}) => {
     const [cart, setCart] = useState([]);
 
     const handleClick = () => {
@@ -12,14 +12,14 @@ const ProductItem = () => {
     return(
         <div className="product-card">
             <img 
-            src="https://images.pexels.com/photos/2263815/pexels-photo-2263815.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
-            alt="" 
+            src={product.images[0]} 
+            alt={product.title} 
             className="product-img"
             />
             <div className="product-info"> 
                 <div className="product-info-text">
-                    <p >$120,00</p>
-                    <p>Game Boy</p>
+                    <p >${product.price}</p>
+                    <p>{product.title}</p>
                 </div>
                 <div>
                     <figure onClick={handleClick} className="produc-info-figure">
